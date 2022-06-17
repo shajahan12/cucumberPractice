e7e890de814ca794fbbbbf569a21de1f34dc3bbc
Shaz <sashajahan.gmail.com>
2022-06-16 19:04:30 -0500

    testing

--- src/test/java/runnerFiles/runTestDemo.java
+++ src/test/java/runnerFiles/runTestDemo.java
@@ -10,7 +10,12 @@ import io.cucumber.junit.CucumberOptions;
 		// path of feature file
 		features = "src/test/java/FeatureFiles/TestDemoFeature.feature",
 		// path of step definition file
-		glue = "stepdefination")
+		glue = "stepdefination",
+		monochrome=true,
+		plugin= {"pretty", "junit:target/JUnitReports/report.xml",
+				"json:target/JSONReports/report.json",
+				"html:target/HtmlReports/report.html"}
+		)
 
 public class runTestDemo {
 	
